e4aedbd71569125c23ee57c5fed4c19e76f2e38d
wos <wizardofsource@protonmail.com>
2020-03-26 15:25:34 +0100

    Added PID control for pendulum.

--- kyvadlo_simulacia/kyvadlo.m
+++ kyvadlo_simulacia/kyvadlo.m
@@ -1,4 +1,5 @@
-
+close all;
+clearvars;
 state = [0, 0];
 Tsim = inf;
 t = 0;
@@ -16,10 +17,32 @@ sz2 = 1;
 sz3 = 0.2;
 k1 = 1;
 k2 = 1;
-r = pi/3;
+r = 1;
+
+% PID parameters
+% These equations must be true, for P I D to be positive
+% g/L < (p1*p2 + p2*p3 + p3*p1)
+% B/m/L^2 > (p1 + p2 + p3)
+% let p=p1=p2=p3
+% then the condition is p < B/m/L^2/3 && g/L/3 < p^2
+% which are satisfied with p < 0 && p < -sqrt(g/L/3)
+
+h = 0.1; % PID parameter [!should work for every h > 0]
+p = -sqrt(g/L/3) - h % p is the real pole locaiton
+p1=p;
+p2=p;
+p3=p;
+
+PIDState = [0 0]; % Integrator state and old e
 
 while t < Tsim 
-    [state,output] = pendulum_step(state, linearization_law(control_law(r, state, k1, k2), state, m, L, g, B), dt, m, L, g, B);
+%   [state,output] = pendulum_step(state, linearization_law(control_law(r, state, k1, k2), state, m, L, g, B), dt, m, L, g, B);
+
+    % Controller
+    [PIDState, u] = PIDAtStableEquilibrium(r, dt, PIDState, state, m, L, g, B, p1, p2, p3);
+    % System
+    [state,output] = pendulum_step(state, u, dt, m, L, g, B);
+    
     t = t + dt;
     tPlot = tPlot + dt;
     if (tPlot > dtPlot)
@@ -60,4 +83,34 @@ function h = circle(ax,x,y,r)
   yunit = r * sin(th) + y;
   h = plot(ax, xunit, yunit, 'k');
   hold off
+end
+
+function [PIDState, u]= PIDAtUnstableEquilibrium(r, deltatime, PIDState, state, m, L, g, B, p1, p2, p3)
+    I = -p1*p2*p3;
+    P = g/L + (p1*p2 + p2*p3 + p3*p1);
+    D = -B/m/L^2 - (p1 + p2 + p3);
+
+    e = r - state(1);
+    de = (e - PIDState(2))/deltatime;
+    eint = PIDState(1) + deltatime*e;
+    u = m*L^2*(P*e + D*de + I*eint);
+    
+    % Update pid state
+    PIDState(1) = eint;
+    PIDState(2) = e;
+end
+
+function [PIDState, u]= PIDAtStableEquilibrium(r, deltatime, PIDState, state, m, L, g, B, p1, p2, p3)
+    I = -p1*p2*p3;
+    P = -g/L + (p1*p2 + p2*p3 + p3*p1);
+    D = -B/m/L^2 - (p1 + p2 + p3);
+
+    e = r - state(1);
+    de = (e - PIDState(2))/deltatime;
+    eint = PIDState(1) + deltatime*e;
+    u = m*L^2*(P*e + D*de + I*eint);
+    
+    % Update pid state
+    PIDState(1) = eint;
+    PIDState(2) = e;
 end
